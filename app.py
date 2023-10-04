b541578a25bda3b67ad17d14f8a5c1e980df28de
Abhinav1541 <abhi1541singh@gmail.com>
2023-10-04 16:25:48 +0530

    added allow_unsafe_werkzeug=True

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def handle_image(image_data):
 #     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    socketio.run(app, port=5000)
+    socketio.run(app, port=5000, allow_unsafe_werkzeug=True)
