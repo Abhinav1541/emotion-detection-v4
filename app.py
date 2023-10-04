d97f74671685e97fff90eab121ecf2c944b735c2
Abhinav1541 <abhi1541singh@gmail.com>
2023-10-04 15:00:59 +0530

    added client side video rendering

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, emit
 import numpy as np
 import cv2
 from tensorflow.keras.models import Sequential
@@ -8,73 +9,128 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import base64
 
-model = Sequential()
+class emotionDetectionModel:
+    def __init__(self):
+        self.model = Sequential()
+        self.build_model()
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+    def build_model(self):
+        self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+        self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
 
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
 
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7, activation='softmax'))
+        self.model.add(Flatten())
+        self.model.add(Dense(1024, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(7, activation='softmax'))
+
+    def load_weights(self, weights_path):
+        self.model.load_weights(weights_path)
+
+    def predict(self, cropped_img):
+        return self.model.predict(cropped_img)
 
 
+model = emotionDetectionModel()
+
+# Load weights
 model.load_weights('model.h5')
 
 # prevents openCL usage and unnecessary logging messages
 cv2.ocl.setUseOpenCL(False)
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 # dictionary which assigns each label an emotion (alphabetical order)
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
+# Initialize Flask app and SocketIO
 app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config["SECRET_KEY"] = "secret"
+socketio = SocketIO()
+socketio.init_app(app)
+
+
+# Serve the HTML page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def generate_frames():
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+@socketio.on("connect")
+def handle_connect():
+    print("client connected")
 
-        frame = cv2.flip(frame, 1)
+# Define a WebSocket route for receiving frames from the client
+@socketio.on('image')
+def handle_image(image_data):
+    # Convert base64 image data to OpenCV format
+    image_bytes = base64.b64decode(image_data.split(',')[1])
+    image_np = np.frombuffer(image_bytes, dtype=np.uint8)
+    frame = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+
+    # Perform emotion detection on 'frame' here
+    frame = cv2.flip(frame, 1)
+    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+
+    # Annotate the frame with detected emotions
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    # Convert the annotated frame to base64 and send it back to the client
+    # cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+    _, buffer = cv2.imencode('.jpg', frame)
+    frame_bytes = buffer.tobytes()
+    annotated_frame_data = 'data:image/jpeg;base64,' + base64.b64encode(frame_bytes).decode('utf-8')
+    emit('annotated_frame', annotated_frame_data)
+
+
+# def generate_frames():
+#     cap = cv2.VideoCapture(0)
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         frame = cv2.flip(frame, 1)
         
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+#         facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+#         for (x, y, w, h) in faces:
+#             cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+#             roi_gray = gray[y:y + h, x:x + w]
+#             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+#             prediction = model.predict(cropped_img)
+#             maxindex = int(np.argmax(prediction))
+#             cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
+#         # Instead of displaying the frame, yield it as a response
+#         _, buffer = cv2.imencode('.jpg', frame)
+#         frame_bytes = buffer.tobytes()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
         
-        # ... (rest of the frame processing code, same as in your original script)
-
-        # Instead of displaying the frame, yield it as a response
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-        
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, port=5000)
